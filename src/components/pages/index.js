404497bbea6879e2cf697727d1961d49622ab5a6
Aman <aman@sensenuts.com>
2019-09-10 18:04:37 +0530

    first page

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,24 +1,53 @@
-import React, { Component } from 'react'
-import logo from '../images/mon.png'
+import React, { Component } from "react";
+import logo from "../images/mon.png";
 export default class Home extends Component {
   render() {
     return (
-      <div >
-        <img style={{position:"relative",height:window.innerHeight,width:window.innerWidth}} src={logo}/>
-        <div class="container" style={{position:"absolute",top:"270px",left:"550px"}}>
-        <div class="pmd-intro-container d-flex row justify-content-start">
-                <div class="align-self-center col-lg-6">
-                    <p style={{color:"white"}}class="lead mb-0 pmd-text-before-line-dark">
-                        Want new way to Access Your Database?
-                    </p>
-                    <h1 style={{color:"white"}} class="pb-3 text-uppercase pmd-display4">
-                        Come to us <br></br>
-                       And Connect Now
-                    </h1>
-                    <button style={{color:"white"}} class="btn btn-outline-light pmd-ripple-effect">Connect</button>
-                </div>
-            </div></div>
+      <div
+        style={{
+          height: window.innerHeight,
+          width: window.innerWidth,
+          backgroundImage: `url(${logo})`
+        }}
+      >
+        <nav
+          style={{ height: "100px" }}
+          class="navbar navbar-expand-sm purple-gradient"
+        ></nav>
+        {/* <img
+          style={{
+            position: "relative",
+            height: window.innerHeight,
+            width: "100%"
+          }}
+          src={logo}
+        /> */}
+        <div class="container">
+          <div class="pmd-intro-container d-flex row justify-content-start">
+            <div style={{ left: "150px" }} class="align-self-center col-lg-6">
+              <p
+                style={{ color: "white" }}
+                class="lead mb-0 pmd-text-before-line-dark"
+              >
+                Want new way to Access Your Database?
+              </p>
+              <h1
+                style={{ color: "white" }}
+                class="pb-3 text-uppercase pmd-display4"
+              >
+                Come to us <br></br>
+                And Connect Now
+              </h1>
+              <button
+                style={{ color: "white" }}
+                class="btn btn-outline-light pmd-ripple-effect"
+              >
+                Connect
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
